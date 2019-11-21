8724b3a9910853efa42826efb985faace1aef8ad
Carl Barringer <cbarringer1@gmail.com>
2019-11-21 15:00:46 -0600

    initial commit

---
+++ Models/Animals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityIntro.Models
+{
+    public class Animals
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Species { get; set; }
+
+        public string color { get; set; }
+        public string ApplicationUserId { get; set; }
+
+        [Display(Name= "Owner")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+
+    }
+}
